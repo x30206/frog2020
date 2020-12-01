02d34dcfdeae92ffbd20531273d071cbe9b590c0
x30206 <71304445+x30206@users.noreply.github.com>
2020-12-01 11:54:07 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://x30206.github.io/frog2020/';
 
 
 function preload() {
